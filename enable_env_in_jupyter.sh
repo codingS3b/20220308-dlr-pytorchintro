41cc20372817c54bfc3703ae0c49189b00ffc3bc
Sebastian Starke <s.starke@hzdr.de>
2023-08-11 15:32:49 +0200

    Fix jupyter kernel installation script

--- enable_env_in_jupyter.sh
+++ enable_env_in_jupyter.sh
@@ -1,1 +1,1 @@
-/projects/p038/p_scads_trainings/BIAS/pytorch_intro/torch_intro_env -m ipykernel install --user --name torch_intro_env --display-name="torch_intro_env"
+/projects/p038/p_scads_trainings/BIAS/torch_intro_env/bin/python -m ipykernel install --user --name torch_intro_env --display-name="torch_intro_env"
